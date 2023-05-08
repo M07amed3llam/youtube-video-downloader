6d47efae837b884a45c57663c84b249107f1a78e
Mohamed Allam <mohamedmamdouh392000@gmail.com>
2023-05-08 14:36:54 +0300

    first commit

---
+++ Video Downloader/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Video_Downloader
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.Write("Enter the URL of the video: ");
+            string url = Console.ReadLine();
+
+            Console.Write("Enter the title of the video: ");
+            string title = Console.ReadLine();
+
+            Console.Write("Enter the path where you want to save the video: ");
+            string path = Console.ReadLine();
+
+            var video = new Video(url);
+            await video.Download(title, path);
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Video Downloader/Video.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using YoutubeExplode;
+using YoutubeExplode.Videos.Streams;
+
+namespace Video_Downloader
+{
+    class Video
+    {
+        private string _url;
+
+        public Video(string url)
+        {
+            _url = url;
+        }
+
+        public async Task Download(string title, string path)
+        {
+            var youtube = new YoutubeClient();
+
+            var video = await youtube.Videos.GetAsync(_url);
+            var streamManifest = await youtube.Videos.Streams.GetManifestAsync(video.Id);
+
+            var streamInfo = streamManifest.GetMuxedStreams().GetWithHighestVideoQuality();
+            var stream = await youtube.Videos.Streams.GetAsync(streamInfo);
+
+            string fileName = $"{title}.{streamInfo.Container.Name}";
+            string filePath = Path.Combine(path, fileName);
+
+            using (var output = File.OpenWrite(filePath))
+            {
+                await stream.CopyToAsync(output);
+            }
+
+            Console.WriteLine($"Video '{title}' has been downloaded to {filePath}.");
+        }
+    }
+}
